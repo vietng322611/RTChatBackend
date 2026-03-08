8b51846b48d6472a9f8f26feda00627408305112
vietng322611 <vietng322611@gmail.com>
2026-03-08 12:27:13 +0700

    implement `CreateAsync`

--- Application/Services/UserService.cs
+++ Application/Services/UserService.cs
@@ -1,12 +1,55 @@
-﻿using RTChatBackend.Application.Interfaces;
+﻿using System.Text.Json;
+using RTChatBackend.Application.DTOs;
+using RTChatBackend.Application.Interfaces;
+using RTChatBackend.Core.Models;
 
 namespace RTChatBackend.Application.Services;
 
 public class UserService: IUserService
 {
-    public Task<UserDto> CreateAsync(string username)
+    private readonly IUserSessionService _userSessionService;
+    private readonly ICodeGenerator _codeGenerator;
+    private readonly int _sessionTtl;
+
+    public UserService(
+        IConfiguration config,
+        IUserSessionService userSessionService,
+        ICodeGenerator codeGenerator)
     {
-        throw new NotImplementedException();
+        if (!int.TryParse(config["Redis:SessionTtl"], out _sessionTtl))
+        {
+            throw new ArgumentException("Invalid SessionTtl", nameof(config));
+        }
+        _userSessionService = userSessionService;
+        _codeGenerator = codeGenerator;
+    }
+    
+    public async Task<UserDto> CreateAsync(string username)
+    {
+        if (string.IsNullOrWhiteSpace(username))
+        {
+            throw new ArgumentException("Username cannot be empty.", nameof(username));
+        }
+
+        var user = new User
+        {
+            UserId = Guid.NewGuid(),
+            Username = username,
+            LoginCode = _codeGenerator.GenerateSessionCode()
+        };
+
+        var data = JsonSerializer.Serialize(user);
+        
+        await _userSessionService.SetTemporaryUserAsync(
+            user.UserId,
+            data,
+            TimeSpan.FromMinutes(_sessionTtl));
+
+        return new UserDto
+        {
+            Username = user.Username,
+            LoginCode = user.LoginCode
+        };
     }
 
     public Task<UserDto?> GetByUsernameAsync(string username)
