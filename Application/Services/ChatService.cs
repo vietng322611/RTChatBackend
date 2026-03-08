fcd8c910b1bcc0c294353545eb924fabae0f6586
vietng322611 <vietng322611@gmail.com>
2026-03-08 12:56:13 +0700

    add `ChatService.cs`

---
+++ Application/Services/ChatService.cs
@@ -0,0 +1,17 @@
+﻿using RTChatBackend.Application.DTOs;
+using RTChatBackend.Application.Interfaces;
+
+namespace RTChatBackend.Application.Services;
+
+public class ChatService: IChatService
+{
+    public Task<ChatDto> GetOrCreateAsync(Guid user1Id, Guid user2Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ChatDto>> GetUserChatsAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
