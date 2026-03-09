c727a1ecb53548ecf09ebc8912b288a1cf9d7dda
vietng322611 <vietng322611@gmail.com>
2026-03-09 08:41:32 +0700

    register services and controllers

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using RTChatBackend.Application.Interfaces;
+using RTChatBackend.Application.Services;
 using RTChatBackend.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRedisInfrastructure(builder.Configuration);
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IChatService, ChatService>();
+builder.Services.AddScoped<IMessageService, MessageService>();
+
+builder.Services.AddControllers();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
