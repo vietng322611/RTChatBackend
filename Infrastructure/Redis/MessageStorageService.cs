5af7a4941861c9210a39479435332484cabd2e06
vietng322611 <vietng322611@gmail.com>
2026-03-09 08:15:19 +0700

    implement `SendMessageAsync`

--- Infrastructure/Redis/MessageStorageService.cs
+++ Infrastructure/Redis/MessageStorageService.cs
@@ -1,6 +1,40 @@
-﻿namespace RTChatBackend.Infrastructure.Redis;
+﻿using System.Text.Json;
+using RTChatBackend.Application.Interfaces;
+using RTChatBackend.Core.Models;
+using StackExchange.Redis;
 
-public class MessageStorageService
+namespace RTChatBackend.Infrastructure.Redis;
+
+public class MessageStorageService(RedisConnectionFactory factory): IMessageStorageService
 {
+    private readonly IDatabase _redis = factory.Connection.GetDatabase();   
     
+    public async Task<Message> SendMessageAsync(Guid chatId, Guid senderId, string content)
+    {
+        var messageId = Guid.NewGuid();
+        var message = new Message
+        {
+            Id = messageId,
+            ChatId = chatId,
+            SenderId = senderId,
+            Content = content,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        var serialized = JsonSerializer.Serialize(message);
+        var created = await _redis.StringSetAsync($"message:{messageId}", serialized, when: When.NotExists);
+        if (!created)
+        {
+            throw new Exception("Failed to store message.");
+        }
+        
+        await _redis.SetAddAsync($"chat:messages:{chatId}", messageId.ToString());
+        
+        return message;
+    }
+
+    public Task<List<Message>> GetMessagesAsync(Guid chatId)
+    {
+        throw new NotImplementedException();
+    }
 }
