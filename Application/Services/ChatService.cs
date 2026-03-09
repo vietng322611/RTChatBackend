fec3767c41f6d3ffaa94bbb2d52bbf119a8a95bf
vietng322611 <vietng322611@gmail.com>
2026-03-09 08:04:53 +0700

    implement `GetOrCreateAsync`

--- Application/Services/ChatService.cs
+++ Application/Services/ChatService.cs
@@ -3,11 +3,24 @@ using RTChatBackend.Application.Interfaces;
 
 namespace RTChatBackend.Application.Services;
 
-public class ChatService: IChatService
+public class ChatService(
+    IChatStorageService chatStorage
+    ): IChatService
 {
-    public Task<ChatDto> GetOrCreateAsync(Guid user1Id, Guid user2Id)
+    public async Task<ChatDto> GetOrCreateAsync(Guid uid1, Guid uid2)
     {
-        throw new NotImplementedException();
+        // Skip verify userIds here
+        var firstUid = uid1.CompareTo(uid2) < 0 ? uid1 : uid2;
+        var secondUid = uid1.CompareTo(uid2) < 0 ? uid2 : uid1;
+
+        var chat = await chatStorage.GetOrCreateChatAsync(firstUid, secondUid);
+        return new ChatDto
+        {
+            Id = chat.Id,
+            User1Id = chat.Uid1,
+            User2Id = chat.Uid2,
+            CreatedAt = chat.CreatedAt
+        };
     }
 
     public Task<List<ChatDto>> GetUserChatsAsync(Guid userId)
