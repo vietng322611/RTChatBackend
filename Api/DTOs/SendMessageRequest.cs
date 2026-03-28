fbb126d7b5588068ebaeea1bdb874549c36db528
vietng322611 <vietng322611@gmail.com>
2026-03-28 11:41:23 +0700

    remove extra character

--- Api/DTOs/SendMessageRequest.cs
+++ Api/DTOs/SendMessageRequest.cs
@@ -4,5 +4,5 @@ public sealed class SendMessageRequest
 {
     public Guid ChatId { get; set; }
     public Guid SenderId { get; set; }
-    public string Content { get; set; } = string.Empty;w
+    public string Content { get; set; } = string.Empty;
 }
