5cf23c5dfcddbde19cd7f13e55f7be058ea3c7d5
vietng322611 <vietng322611@gmail.com>
2026-03-28 11:40:36 +0700

    add `GetAsync` and implement `GetUserChatsAsync`

--- Application/Interfaces/IChatService.cs
+++ Application/Interfaces/IChatService.cs
@@ -4,7 +4,7 @@ namespace RTChatBackend.Application.Interfaces;
 
 public interface IChatService
 {
+    Task<ChatDto?> GetAsync(Guid chatId);
     Task<ChatDto> GetOrCreateAsync(Guid uid1, Guid uid2);
-
     Task<List<ChatDto>> GetUserChatsAsync(Guid userId);
 }
--- Application/Services/ChatService.cs
+++ Application/Services/ChatService.cs
@@ -7,13 +7,11 @@ public class ChatService(
     IChatStorageService chatStorage
     ): IChatService
 {
-    public async Task<ChatDto> GetOrCreateAsync(Guid uid1, Guid uid2)
+    public async Task<ChatDto?> GetAsync(Guid chatId)
     {
-        // Skip verify userIds here
-        var firstUid = uid1.CompareTo(uid2) < 0 ? uid1 : uid2;
-        var secondUid = uid1.CompareTo(uid2) < 0 ? uid2 : uid1;
+        var chat = await chatStorage.GetAsync(chatId);
+        if (chat == null) return null;
 
-        var chat = await chatStorage.GetOrCreateChatAsync(firstUid, secondUid);
         return new ChatDto
         {
             Id = chat.Id,
@@ -23,8 +21,31 @@ public class ChatService(
         };
     }
 
-    public Task<List<ChatDto>> GetUserChatsAsync(Guid userId)
+    public async Task<ChatDto> GetOrCreateAsync(Guid uid1, Guid uid2)
     {
-        throw new NotImplementedException();
+        // Skip verify userIds here
+        var firstUid = uid1.CompareTo(uid2) < 0 ? uid1 : uid2;
+        var secondUid = uid1.CompareTo(uid2) < 0 ? uid2 : uid1;
+
+        var chat = await chatStorage.GetOrCreateAsync(firstUid, secondUid);
+        return new ChatDto
+        {
+            Id = chat.Id,
+            User1Id = chat.Uid1,
+            User2Id = chat.Uid2,
+            CreatedAt = chat.CreatedAt
+        };
+    }
+
+    public async Task<List<ChatDto>> GetUserChatsAsync(Guid userId)
+    {
+        var chats = await chatStorage.GetUserChatsAsync(userId);
+        return chats.Select(chat => new ChatDto
+        {
+            Id = chat.Id,
+            User1Id = chat.Uid1,
+            User2Id = chat.Uid2,
+            CreatedAt = chat.CreatedAt
+        }).ToList();
     }
 }
