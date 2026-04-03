1df31cb3fbde121ce2b6816f469cf861a04d9f98
vietng322611 <vietng322611@gmail.com>
2026-04-03 15:43:08 +0700

    remove all `ttl` parameter and add `GetAllUsernameAsync` method

--- Application/Interfaces/IUserSessionService.cs
+++ Application/Interfaces/IUserSessionService.cs
@@ -2,11 +2,12 @@
 
 public interface IUserSessionService
 {
-    Task SetTemporaryUserAsync(Guid userId, string data, TimeSpan expiry);
+    Task SetTemporaryUserAsync(Guid userId, string data);
     Task<bool> IsUsernameTakenAsync(string username);
-    Task SetUsernameMappingAsync(string username, Guid userId, TimeSpan expiry);
-    Task SetLoginCodeMappingAsync(string loginCode, Guid userId, TimeSpan expiry);
+    Task SetUsernameMappingAsync(string username, Guid userId);
+    Task SetLoginCodeMappingAsync(string loginCode, Guid userId);
     Task<Guid?> GetUserIdByUsernameAsync(string username);
     Task<Guid?> GetUserIdByLoginCodeAsync(string loginCode);
     Task<string?> GetUserDataAsync(Guid userId);
+    Task<List<string>> GetAllUsernameAsync();
 }
