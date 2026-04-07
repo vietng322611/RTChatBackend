f3e67e5ca2325591f63f30fe4a96780e7f0607ff
vietng322611 <vietng322611@gmail.com>
2026-04-07 11:45:46 +0700

    pass connection directly for testing

--- Infrastructure/Redis/RedisConnectionFactory.cs
+++ Infrastructure/Redis/RedisConnectionFactory.cs
@@ -2,10 +2,19 @@
 
 namespace RTChatBackend.Infrastructure.Redis;
 
-public class RedisConnectionFactory(string connectionString)
+public class RedisConnectionFactory
 {
-    private readonly Lazy<IConnectionMultiplexer> _connection = new(()
-        => ConnectionMultiplexer.Connect(connectionString));
+    private readonly IConnectionMultiplexer _connection;
 
-    public IConnectionMultiplexer Connection => _connection.Value;
+    public RedisConnectionFactory(string connectionString)
+    {
+        _connection = ConnectionMultiplexer.Connect(connectionString);
+    }
+
+    public RedisConnectionFactory(IConnectionMultiplexer connection)
+    {
+        _connection = connection;
+    }
+
+    public IConnectionMultiplexer Connection => _connection;
 }
